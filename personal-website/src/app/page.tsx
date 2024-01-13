19abe097b117dbfe61e6a30f67cdca8d915dd1a2
Bryan Matthew <bryanmatthew206@gmail.com>
2024-01-14 02:09:04 +0700

    feat: add project section

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Project from "@/components/Project";
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <About />
+            <Project />
         </div>
     );
 }
---
+++ personal-website/src/components/Project/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import SectionHeading from "../sectionHeading";
+
+interface ProjectProps {}
+
+const Project: FunctionComponent<ProjectProps> = () => {
+    return <SectionHeading title="Projects" />;
+};
+
+export default Project;
