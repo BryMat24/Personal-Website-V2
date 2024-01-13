36b9bccf519a428225e334790b6c7af8bc5b81cf
Bryan Matthew <bryanmatthew206@gmail.com>
2024-01-13 23:18:23 +0700

    feat: create home component

---
+++ personal-website/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Bryan's Space",
+    description: "Bryan Matthew's personal website",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
