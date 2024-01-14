31d2db9390989820c5cb55f55a807387ac5258b8
Bryan Matthew <bryanmatthew206@gmail.com>
2024-01-14 20:52:14 +0700

    style: add blob

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,14 +1,21 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Project from "@/components/Project";
-import Cursor from "@/components/cursor";
+import Footer from "@/components/Footer";
 
 export default function Home() {
+    const blobStyle = {
+        "--time": "20s",
+        "--amount": 5,
+        "--fill": "white",
+    };
+
     return (
         <div>
             <Hero />
             <About />
             <Project />
+            <Footer />
         </div>
     );
 }
--- personal-website/src/components/Hero/index.tsx
+++ personal-website/src/components/Hero/index.tsx
@@ -1,36 +1,36 @@
 import { FunctionComponent } from "react";
 import Header from "./header";
 
-interface Props {}
-
-const Hero: FunctionComponent<Props> = () => {
+const Hero: FunctionComponent = () => {
     return (
-        <header className="mx-8 min-h-12 md:w-9/12 md:m-auto lg:mt-8 my-4 lg:mb-0 lg:min-h-screen xxl:min-h-0 xxl:mb-12">
-            <Header />
-            <div className="flex flex-col w-10/12 lg:w-7/12 lg:mt-5 relative">
-                <div className="overflow-hidden mb-6 lg:text-7xl w-min">
-                    <h1
-                        className="lg:text-8xl md:text-7xl blockf font-semibold"
-                        style={{ lineHeight: 1.15 }}
-                    >
-                        Bryan M Rustardy
-                    </h1>
-                </div>
-                <div>
-                    <div className="overflow-hidden my-2 lg:text-5xl">
-                        <h2
-                            className="text-dark-primary my-2 md:text-4xl lg:text-5xl block font-light"
-                            style={{ lineHeight: 1.2 }}
+        <>
+            <header className="mx-8 min-h-12 md:w-9/12 md:m-auto lg:mt-8 my-4 lg:mb-0 lg:min-h-screen xxl:min-h-0 xxl:mb-12">
+                <Header />
+                <div className="flex flex-col w-10/12 lg:w-7/12 lg:mt-5 relative">
+                    <div className="overflow-hidden mb-6 lg:text-7xl w-min">
+                        <h1
+                            className="lg:text-8xl md:text-7xl blockf font-semibold mix-blend-difference"
+                            style={{ lineHeight: 1.15 }}
                         >
-                            Hi, I'm Bryan! I like creating clean, responsive and
-                            functional web apps.
-                        </h2>
+                            Bryan M Rustardy
+                        </h1>
                     </div>
-                    <div className="w-32 my-1 h-1 bg-dark-secondary mt-5 lg:h-2 lg:w-1/2"></div>
+                    <div>
+                        <div className="overflow-hidden my-2 lg:text-5xl">
+                            <h2
+                                className="text-dark-primary my-2 md:text-4xl lg:text-5xl block font-light"
+                                style={{ lineHeight: 1.2 }}
+                            >
+                                Hi, I'm Bryan! I like creating clean, responsive
+                                and functional web apps.
+                            </h2>
+                        </div>
+                        <div className="w-32 my-1 h-1 bg-dark-secondary mt-5 lg:h-2 lg:w-1/2"></div>
+                    </div>
                 </div>
-            </div>
-            <div className="w-[300px] h-2 my-1 bg-white mt-1"></div>
-        </header>
+                <div className="w-[300px] h-2 my-1 bg-white mt-1"></div>
+            </header>
+        </>
     );
 };
 
