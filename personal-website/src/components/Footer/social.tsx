937299ff83bae770ed3f66804b1f9a3141347ec4
Bryan Matthew <bryanmatthew206@gmail.com>
2024-01-14 21:32:14 +0700

    refactor: put all project related data to /data/project.json

--- personal-website/src/components/Footer/social.tsx
+++ personal-website/src/components/Footer/social.tsx
@@ -13,7 +13,7 @@ const Social: FunctionComponent = () => {
                     <svg
                         stroke="currentColor"
                         fill="currentColor"
-                        stroke-width="0"
+                        strokeWidth="0"
                         role="img"
                         viewBox="0 0 24 24"
                         height="1em"
--- personal-website/src/components/Project/index.tsx
+++ personal-website/src/components/Project/index.tsx
@@ -2,33 +2,29 @@ import React, { FunctionComponent } from "react";
 import SectionHeading from "../sectionHeading";
 import ProjectDisplay from "./project";
 
+interface projectData {
+    title: string;
+    description: string;
+    imageUrl: string;
+}
+
 const Project: FunctionComponent = () => {
+    const projectData = require("../../data/project.json");
+
     return (
         <section className="flex flex-col duration-700 md:w-9/12 md:m-auto md:mt-[180px] px-8">
             <div className="flex my-5 mb-3 lg:mt-0 overflow-hidden bg-dark-bg duration-700 relative py-2">
                 <SectionHeading title="Projects" type="left" />
             </div>
             <div className="mt-8">
-                <ProjectDisplay
-                    title="ShareMySight"
-                    description="An application crafted to support individuals with visual impairments by integrating OpenAI ChatBot for image-to-text communication."
-                    imageUrl="ShareMySight.png"
-                />
-                <ProjectDisplay
-                    title="Shoppable"
-                    description="With user-friendly navigation, secure transactions, discover your style effortlessly on our ecommerce platform for clothing brands."
-                    imageUrl="Ecommerce.png"
-                />
-                <ProjectDisplay
-                    title="Online Food Delivery Predictor"
-                    description="Apply ML models: decision tree, random forest, lasso regression and xgboost to predict time taken for food delivery."
-                    imageUrl="FoodDelivery.jpg"
-                />
-                <ProjectDisplay
-                    title="SortViz"
-                    description="An interactive sorting visualizer, incorporating four sorting algorithms: Bubble Sort, Quick Sort, Heap Sort and Merge Sort"
-                    imageUrl="SortViz.png"
-                />
+                {projectData.map((el: projectData, index: number) => (
+                    <ProjectDisplay
+                        title={el.title}
+                        description={el.description}
+                        imageUrl={el.imageUrl}
+                        key={index}
+                    />
+                ))}
             </div>
         </section>
     );
